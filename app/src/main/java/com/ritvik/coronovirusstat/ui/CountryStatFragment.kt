d7e2fca5d418bc5e2a015687b9fe27a4747d711a
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2020-07-21 02:43:01 +0530

    added circular progress bar

--- app/src/main/java/com/ritvik/coronovirusstat/ui/CountryStatFragment.kt
+++ app/src/main/java/com/ritvik/coronovirusstat/ui/CountryStatFragment.kt
@@ -1,5 +1,6 @@
 package com.ritvik.coronovirusstat.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,15 @@ import androidx.lifecycle.ViewModelProviders
 import com.ritvik.coronovirusstat.databinding.CountryStatLayoutBinding
 import com.ritvik.coronovirusstat.skeleton.CountryStatViewModel
 import com.ritvik.coronovirusstat.skeleton.CountryStatViewModelFactory
+import java.math.RoundingMode
+import kotlin.math.roundToLong
 
 class CountryStatFragment : Fragment() {
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+         val perc: String = "%"
 
         val application = requireNotNull(activity).application
 
@@ -29,6 +35,18 @@ class CountryStatFragment : Fragment() {
             this, countryStatViewModelFactory).get(CountryStatViewModel::class.java)
 
         binding.countryStatVM = countryStatViewModel
+
+        binding.circularProgressBarFatality.apply {
+          val rate = ((countryData.deaths?.div(countryData.cases!!))?.times(100))?.toFloat()!!
+            binding.fatalityRateText.text = rate.toBigDecimal().setScale(2, RoundingMode.HALF_EVEN).toString() + perc
+            setProgressWithAnimation(rate,1000)
+        }
+
+        binding.circularProgressBarRecovery.apply {
+            val rate = ((countryData.recovered?.div(countryData.cases!!))?.times(100))?.toFloat()!!
+            binding.recoveryRateText.text = rate.toBigDecimal().setScale(2, RoundingMode.HALF_EVEN).toString() + perc
+            setProgressWithAnimation(rate,1000)
+        }
 
         return binding.root
     }
